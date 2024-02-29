304a30283934ffcdfcae2b38c5d75b9ac93862f0
Yuri Mikhin <urasergeevich@mail.ru>
2024-02-29 23:55:19 +0300

    Remove vite.svg file and update vite.config.ts
    Removed the unnecessary `vite.svg` file from the public directory. Also, updated the `vite.config.ts` file by changing the input path, entry file names in rollupOptions, and removing the aliases from the resolve option. The configure for tests has also been made simpler by removing exclusions and the 'name' parameter.

--- vite.config.ts
+++ vite.config.ts
@@ -10,29 +10,19 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       input: {
-        api: resolve(__dirname, 'src', 'api', 'index.ts'),
+        api: resolve(__dirname, 'src', 'main.tsx'),
       },
       output: {
-        entryFileNames: '[name].js',
+        entryFileNames: 'code.js',
       },
     },
     target: 'es2015',
   },
   plugins: [react()],
-  resolve: {
-    alias: {
-      '~api': resolve(__dirname, 'src', 'api'),
-      '~test-utils': resolve(__dirname, 'src', 'test-utils'),
-      '~types': resolve(__dirname, 'src', 'types'),
-      '~utils': resolve(__dirname, 'src', 'utils'),
-    },
-  },
   test: {
     coverage: {
       provider: 'v8',
       reportsDirectory: resolve(__dirname, 'coverage', 'api'),
     },
-    exclude: ['node_modules', 'src/ui/**/*.spec.tsx'],
-    name: 'api',
   },
 });
